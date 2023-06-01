5a426b1b90835fe722155e54e01eeed740f1a543
tom <1246068769@qq.com>
2023-06-01 23:20:36 +0800

    我再一次的提交了

--- hello.java
+++ hello.java
@@ -1,6 +1,5 @@
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello World	");
-		System.out.println("www.ava.com");
+		System.out.println("www.Hello.com");
 	}
 }
